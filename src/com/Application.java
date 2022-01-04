c1d48492a43dd5e0401a36133c55f1d1f24a1ff8
Eduard <serbanedi.fr@gmail.com>
2022-01-04 15:59:34 +0100

    Singleton

--- src/com/Application.java
+++ src/com/Application.java
@@ -6,20 +6,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
-//Exercice 1 :
-//Coment pourrait-on éviter de répéter la saisie complète
-// des TextInput de type "O/N" (parce que ça oblige à écrire "O",
-// "N" à chaque fois)
-// input = new TextInput(scanner, "Voulez-vous ajouter des notes (O/N)? ", "O", "N", "o", "n");
-// input = new TextInput(scanner, "Cet étudiant est inconnu. Voulez-vous le créer (O/N)? ", "O", "N", "o", "n");
-
-//Exercice 2 :
-//Faire une classe NoteInput qui contrôle la saisie des notes
 
 public class Application {
-    public static <Toto> Toto getFirst(Toto... array) {
-        return array[1];
-    }
 
     public static void main(String[] args) {
         StudentMap students = new StudentMap();
@@ -32,8 +20,8 @@ public class Application {
             System.out.println("File not found");
         }
 
-        Scanner scanner = new Scanner(System.in);
-        TextInput command = new TextInput(scanner, ">");
+        SingletonScanner singletonScanner = SingletonScanner.getInstance();
+        TextInput command = new TextInput(singletonScanner.getScanner(), ">");
         String name = command.ask();
         Student student;
 
@@ -44,9 +32,9 @@ public class Application {
             System.out.println(String.format("Notes de %s : %s",
                     student.getFullName(),
                     student.getNotes()));
-            askToAddNotes(scanner, student);
+            askToAddNotes(singletonScanner.getScanner(), student);
         } else {
-            askToCreateNewStudent(students, scanner, name);
+            askToCreateNewStudent(students, singletonScanner.getScanner(), name);
         }
 
         System.out.println(students);
